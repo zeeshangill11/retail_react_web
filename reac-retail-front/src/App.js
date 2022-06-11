28c661b53e76bb05f05260bc31c1bb47c5ed5f91
Developer 1 <developer1.oxygensoft@gmail.com>
2022-06-11 19:40:03 +0500

    2022_06_11_sufyan_evening

--- reac-retail-front/src/App.js
+++ reac-retail-front/src/App.js
@@ -7,6 +7,7 @@ import AddTutorial from "./components/add-tutorial.component";
 import Tutorial from "./components/tutorial.component";
 import TutorialsList from "./components/tutorials-list.component";
 import Login from "./components/Login/Login";
+
 import executiveSummary from "./components/executiveSummary/executiveSummary";
 import executiveSummaryOnHandSimple from "./components/executiveSummary/executiveSummaryOnHandSimple";
 import underOvers from "./components/executiveSummary/underOvers";
@@ -15,11 +16,12 @@ import CriticalOutOfStock from "./components/executiveSummary/CriticalOutOfStock
 import executiveSummaryCount from "./components/executiveSummary/executiveSummaryCount";
 import executiveSummaryFront from "./components/executiveSummary/executiveSummaryFront";
 import executiveSummaryBack from "./components/executiveSummary/executiveSummaryBack";
+
 import handheldDevices from "./components/handheldDevices/handheldDevices";
 import edithandhelddevice from "./components/handheldDevices/edithandhelddevice";
 import addhandheldDevice from "./components/handheldDevices/addhandheldDevice";
 
-
+import stockSummary from "./components/stockSummary/stockSummary";
 
 class App extends Component {
 
@@ -40,6 +42,7 @@ class App extends Component {
             <Route exact path="/add" component={AddTutorial} />
             <Route path="/tutorials/:id" component={Tutorial} />
             <Route path="/login" component={Login} />
+            
             <Route path="/executiveSummary" component={executiveSummary} />
             <Route path="/executiveSummaryOnHandSimple" component={executiveSummaryOnHandSimple} />
             <Route path="/underOvers" component={underOvers} />
@@ -48,9 +51,12 @@ class App extends Component {
             <Route path="/executiveSummaryFront" component={executiveSummaryFront} />
             <Route path="/executiveSummaryBack" component={executiveSummaryBack} />
             <Route path="/CriticalOutOfStock" component={CriticalOutOfStock} />
+            
             <Route path="/handheldDevices" component={handheldDevices} />
             <Route path="/edithandhelddevice/:id" component={edithandhelddevice} />
             <Route path="/addhandheldDevice" component={addhandheldDevice} />
+            
+            <Route path="/stockSummary" component={stockSummary} />
             
           </Switch>
           </>
